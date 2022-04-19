380c0469e720e74d1ac3b5327848fa147ea8d4ba
Henry K <h3nrykam@gmail.com>
2022-04-18 21:08:18 -0400

    create dictionary that stores files and hashes

--- fim.ps1
+++ fim.ps1
@@ -1,7 +1,9 @@
 Write-Host ""
 Write-Host "What would you like to do?"
-Write-Host "A) Collect new Baseline?"
-Write-Host "B) Begin monitoring files with saved baseline"
+Write-Host ""
+Write-Host "    A) Collect new Baseline?"
+Write-Host "    B) Begin monitoring files with saved baseline"
+Write-Host ""
 
 $response = Read-Host -Prompt "Please enter 'A' or 'B'"
 # Future: add error checking
@@ -20,17 +22,32 @@ Function Erase-Baseline-If-Already-Exists(){
 
 }
 if ($response -eq "A".ToUpper()) {
+    # Delete baseline.txt if it already exists
+    Erase-Baseline-If-Already-Exists
     # Calculate Hash from the target files and store in baseline.txt
-    Write-Host "Calculate Hashes, make new baseline.txt" -ForegroundColor Cyan
+    # Write-Host "Calculate Hashes, make new baseline.txt" -ForegroundColor Cyan
     # Collect all files in the target folder
     $files = Get-ChildItem -Path .\Files
     # For each file, calculate the hash, and write to baseline.txt
     foreach ($f in $files){
-        Calculate-File-Hash $f.FullName
+        $hash = Calculate-File-Hash $f.FullName
         "$($hash.Path)|$($hash.Hash)" | Out-File -FilePath .\baseline.txt -Append
     }
 }
 elseif ($response -eq "B".ToUpper()) {
+    $fileHashDictionary = @{}
+    # Load file|hash from baseline.txt and store them in a dictionary
+    $filePathesandHashes = Get-Content -Path .\baseline.txt
+
+    foreach ($f in $filePathesandHashes) {
+        $fileHashDictionary.add($f.Split("|")[0], $f.Split("|")[1])
+    }
+
+    # Begin (continuosly) monitoring files with saved Baseline
+    while ($true) {
+        Start-Sleep -Seconds 1
+        Write-Host "Checking if files match..."
+    }
     # Begin monitoring files with saved Baseline
     Write-Host "Read existing baseline.txt, start monitoring files." -ForegroundColor Yellow
 }
